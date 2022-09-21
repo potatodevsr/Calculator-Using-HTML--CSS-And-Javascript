4ccfbdbdf0bf8cdbc28e13b0ff1fbb94e0484513
potatodevsr <hoshiku1997@gmail.com>
2022-09-21 16:42:23 +0700

    script.js1

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("Hello! I am an alert box!!")
+const display1EL = document.querySelector('display-1')
+const display2EL = document.querySelector('display-2')
+const numbersEl = document.querySelectorAll('.number')
+const operationEl = document.querySelectorAll('.operation')
+const equalEl = document.querySelector('.equal')
+const clearAllEl = document.querySelector('.all-clear')
+const clearLastEl = document.querySelector('.last-entity-clear')
+let dis1Num = ''
+let dis2Num = ''
+let result = null
+let lastOperation = ''
+let haveDot = false
+
+numbersEl.forEach((number) => {
+    number.addEventListener('click', (e) => {
+        if (e.target.innerText )
+    })
+})
